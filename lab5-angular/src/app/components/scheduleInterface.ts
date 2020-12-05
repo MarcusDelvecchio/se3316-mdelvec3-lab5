ca2a44d9bcc719ecd0b33c6356d1249bb6a9c67d
Marcus Del Vecchio <marcusdel2112@rogers.com>
2020-12-04 19:21:49 -0500

    Course and Schedule Interfaces

---
+++ lab5-angular/src/app/components/scheduleInterface.ts
@@ -0,0 +1,9 @@
+export interface scheduleInfo {
+    creator: string, 
+    modified: string,   // date format??
+    length: number,
+    description: string, 
+    expanded: false,
+    visibility: string,
+    // deleted: boolean ???
+};
